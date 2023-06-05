346dc7c1c3bc7d2debcec16b54381ff856667820
selvarudra07 <133361912+selvarudra07@users.noreply.github.com>
2023-06-05 14:45:13 +0530

    Update loginPOM.java

--- CucumberNew/src/test/java/pom/loginPOM.java
+++ CucumberNew/src/test/java/pom/loginPOM.java
@@ -11,7 +11,7 @@ public class loginPOM extends global {
 	public loginPOM() 
 	{
 		this.driver=driver;
-		PageFactory.initElements(driver, this);
+		PageFactory.initElements(driver, this.keyword);
 	}
 	
 	
